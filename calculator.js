b155e80c7803ec1ae0e71f679a97d8d2bf81d4d9
guilherme.piovezan <guilherme.piovezan00@gmail.com>
2026-06-02 20:51:31 -0300

    feat: add function operator

---
+++ calculator.js
@@ -0,0 +1,17 @@
+function Calculator(operator, a, b) {
+  switch (operator) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      if (b === 0) {
+        throw new Error('Division by zero is not allowed.');
+      }
+      return a / b;
+    default:
+      throw new Error('Invalid operator. Supported operators are: +, -, *, /.');
+  }
+}
