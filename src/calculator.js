6537ac5fafde6a3702bac043e3700aa0a59a60e2
guilherme.piovezan <guilherme.piovezan00@gmail.com>
2026-06-02 21:40:15 -0300

    feat: add modulo operator support to calculator function

--- src/calculator.js
+++ src/calculator.js
@@ -11,6 +11,8 @@ function calculator(operator, a, b) {
         throw new Error('Division by zero is not allowed.');
       }
       return a / b;
+    case '%':
+      return a % b;
     default:
       throw new Error('Invalid operator. Supported operators are: +, -, *, /.');
   }
